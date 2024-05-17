d1787d11f6c8058ddf78e49284190ac022effdf9
Igiz <igizstarkle@gmail.com>
2024-05-17 14:47:17 +0300

    Initial commit

---
+++ lib/screens/MenuRoute.dart
@@ -0,0 +1,10 @@
+ import 'package:flutter/cupertino.dart';
+
+class MenuRoute extends StatelessWidget {
+   const MenuRoute();
+
+   @override
+   Widget build(BuildContext context) {
+     return const Placeholder();
+   }
+ }
